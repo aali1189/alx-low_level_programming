142c9df8da158838ed30781e8320d2ff04a11af4
Amro Madani <33586588+aali1189@users.noreply.github.com>
2023-09-14 23:29:47 +0300

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,4 +12,3 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /* MAIN_H */
