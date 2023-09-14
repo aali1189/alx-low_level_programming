bb2561948a09aba0041ec86b51f8ae9cb6ea5c67
Amro Madani <Aali1189@gmail.com>
2023-09-14 12:10:26 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void print_numbers(void)
+{
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
