1efae4e83a6d7b4c42d1c24145b484322a8c7548
Amro Madani <33586588+aali1189@users.noreply.github.com>
2023-09-15 04:02:16 +0300

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - prints
+ *
+ * Return: void
+ */
+
+
 void print_most_numbers(void)
 {
-int number = 48;
- for (number = 48; number < 58; number++)
-{
- if (number == 50 || number == 52)
- {
-  continue;
- }
-_putchar(number);
-}
-_putchar(10);
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
 }
